b1cf5e0f87b4092e924672dce4154c19d2cad908
Simon Walker <github@helpmebot.org.uk>
2010-12-20 03:21:34 +0000

    commit a metric ton of stuff :)

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+require_once("config.inc.php");
+
+// the web-accessible folder where index.php is stored
+$baseFilePath = "/";
+
+// the real file path where index.php is stored
+$baseScriptPath = "/var/www/solar/";
+
+$s_cacheDir = $baseScriptPath . '/smartycache/';
+$s_compileDir = $baseScriptPath . '/smartycompile/';
+$s_templateDir = $baseScriptPath . '/template/';
+$s_configDir = $baseScriptPath . '/smartyconfig/';
+
