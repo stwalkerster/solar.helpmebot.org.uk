0b50fe010d0d67fe5d963fc9d40098655c510f6f
Simon Walker <github@helpmebot.org.uk>
2010-12-20 14:06:13 +0000

    fix

--- graph/buildAll.cmd
+++ graph/buildAll.cmd
@@ -1,58 +1,58 @@
 echo "Processing all examples"
 echo " [01/28] A simple line chart"
-php -q %~dp0Example1.php
+php -q Example1.php
 echo " [02/28] A cubic curve graph"
-php -q %~dp0Example2.php
+php -q Example2.php
 echo " [03/28] An overlayed bar graph"
-php -q %~dp0Example3.php
+php -q Example3.php
 echo " [04/28] Showing how to draw area"
-php -q %~dp0Example4.php
+php -q Example4.php
 echo " [05/28] A limits graph"
-php -q %~dp0Example5.php
+php -q Example5.php
 echo " [06/28] A simple filled line graph"
-php -q %~dp0Example6.php
+php -q Example6.php
 echo " [07/28] A filled cubic curve graph"
-php -q %~dp0Example7.php
+php -q Example7.php
 echo " [08/28] A radar graph"
-php -q %~dp0Example8.php
+php -q Example8.php
 echo " [09/28] Showing how to use labels"
-php -q %~dp0Example9.php
+php -q Example9.php
 echo " [10/28] A 3D exploded pie graph"
-php -q %~dp0Example10.php
+php -q Example10.php
 echo " [11/28] A true bar graph"
-php -q %~dp0Example12.php
+php -q Example12.php
 echo " [12/28] A 2D exploded pie graph"
-php -q %~dp0Example13.php
+php -q Example13.php
 echo " [13/28] A smooth flat pie graph"
-php -q %~dp0Example14.php
+php -q Example14.php
 echo " [14/28] Playing with line style and pictures inclusion"
-php -q %~dp0Example15.php
+php -q Example15.php
 echo " [15/28] Importing CSV data"
-php -q %~dp0Example16.php
+php -q Example16.php
 echo " [16/28] Playing with axis"
-php -q %~dp0Example17.php
+php -q Example17.php
 echo " [17/28] Missing values"
-php -q %~dp0Example18.php
+php -q Example18.php
 echo " [18/28] Error reporting"
-php -q %~dp0Example19.php
+php -q Example19.php
 echo " [19/28] Stacked bar graph"
-php -q %~dp0Example20.php
+php -q Example20.php
 echo " [20/28] Playing with background"
-php -q %~dp0Example21.php
+php -q Example21.php
 echo " [21/28] Customizing plot charts"
-php -q %~dp0Example22.php
+php -q Example22.php
 echo " [22/28] Playing with background - Bis"
-php -q %~dp0Example23.php
+php -q Example23.php
 echo " [23/28] X Versus Y chart"
-php -q %~dp0Example24.php
+php -q Example24.php
 echo " [24/28] Using shadows"
-php -q %~dp0Example25.php
+php -q Example25.php
 echo " [25/28] Two Y axis / shadow demonstration"
-php -q %~dp0Example26.php
+php -q Example26.php
 echo " [26/28] Naked and easy!"
-php -q %~dp0Naked.php
+php -q Naked.php
 echo " [27/28] Let's go fast, draw small!"
-php -q %~dp0SmallGraph.php
+php -q SmallGraph.php
 echo " [28/28] A Small stacked chart"
-php -q %~dp0SmallStacked.php
+php -q SmallStacked.php
 echo "Rendering complete!"
