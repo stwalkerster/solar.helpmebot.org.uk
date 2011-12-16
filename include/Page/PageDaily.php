e3971d58dae539d8aef1ca9e410eca0648f99fb2
Simon Walker <github@stwalkerster.co.uk>
2011-12-16 07:53:26 +0000

    Actually restrict it to the past 7 days

--- include/Page/PageDaily.php
+++ include/Page/PageDaily.php
@@ -9,14 +9,14 @@ class PageDaily extends PageBase
 		$params = array();
 		$query = 'SELECT TIME(timestamp) AS x, generation as y FROM `solar`.`hourlydata` WHERE timestamp LIKE :yesterday;';
 
-		for ($i = -1; $i > -29; $i--) {		
+		for ($i = -1; $i > -8; $i--) {		
 			$yesterday = date("Y-m-d", mktime(0,0,0,date("m"),date("d")+ $i,date("Y"))) ;
 			$params[] = $yesterday;
 		}
 		
 		$graphs = PageBase::createGraph($query, $params, "day");
 	
-		$this->mSmarty->assign('graphlisttitle', "Last 28 days power generation");
+		$this->mSmarty->assign('graphlisttitle', "Last 7 days power generation");
 		$this->mSmarty->assign('graphlist', $graphs);
 		$this->mSmarty->assign('content', 'MainPage.tpl');
 	}
