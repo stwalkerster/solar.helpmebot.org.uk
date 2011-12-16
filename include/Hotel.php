13316d1872143c2bcce90e326469abe17a5221fb
Simon Walker <github@stwalkerster.co.uk>
2011-12-16 06:54:18 +0000

    fix

--- include/Hotel.php
+++ include/Hotel.php
@@ -20,7 +20,7 @@ class Hotel
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>The Blackfish Hotel</title>
+		<title>Oops!</title>
 		<style type="text/css">
 			#error{display:block;clear:both;font-size:x-small;}
 			p{margin:0px;padding:0px;}
